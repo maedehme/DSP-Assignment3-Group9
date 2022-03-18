8b53300918feac7a7a673b0c3fd86639a600a213
Maedeh <shakiba_mehdizadeh@yahoo.com>
2022-03-18 11:42:59 +1100

    Add files via upload

---
+++ numeric.py
@@ -0,0 +1,66 @@
+# To be filled by students
+from dataclasses import dataclass
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+@dataclass
+class NumericColumn:
+
+  col_name: str
+  serie: pd.Series
+ 
+  def get_name(self):
+    return self.col_name
+
+  def get_unique(self):
+    return self.serie.unique().shape[0]
+
+  def get_missing(self):
+    return self.serie.isnull().sum()
+
+  def get_zeros(self):
+    return self.serie[self.serie == 0].count()
+
+  def get_negatives(self):
+    return self.serie[self.serie < 0].count()
+
+  def get_mean(self):
+    return self.serie.mean()
+
+  def get_std(self):
+    return self.serie.std()
+  
+  def get_min(self):
+    return self.serie.min()
+
+  def get_max(self):
+    return self.serie.max()
+
+  def get_median(self):
+    return self.serie.median()
+
+  def get_histogram(self):
+   return plt.hist(self.serie, bins = 50)
+
+  def get_frequent(self):
+
+    temp = self.serie.unique()
+    temp = temp[::-1][:20]
+
+    occurrence = []
+    percentage = []
+
+    for val in temp:
+
+      cnt = self.serie[self.serie == val].count()
+      occurrence.append(cnt)
+      percentage.append(cnt / self.serie.shape[0])
+
+    df = pd.DataFrame()
+    df['value'] = temp
+    df['occurrences'] = occurrence
+    df['percentage'] = percentage
+
+    return df.sort_values('occurrences', ascending=False)
